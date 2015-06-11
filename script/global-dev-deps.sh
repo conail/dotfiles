3e9e2193c9c185e7772a723985fc6db0e05ed4fc
Conail Stewart <conail@conail.net>
2015-06-11 18:26:46 -0400

    Add javascript utilities.

--- script/global-dev-deps.sh
+++ script/global-dev-deps.sh
@@ -1,16 +1,16 @@
 sudo apt-get update
 
 # Common Development Libraries
-sudo apt-get install build-essential tree
+sudo apt-get install build-essential tree cmake ctags
 
 # Network
-sudo apt-get install netcat whois
+sudo apt-get install netcat whois wget curl
 
 # Source Control
 sudo apt-get install git svn
 
 # Editors
-sudo apt-get install vim emacs
+sudo apt-get install vim emacs23-nox
 
 # Third-party Utilities
 sudo apt-get install heroku-toolbelt
@@ -19,4 +19,7 @@ sudo apt-get install heroku-toolbelt
 sudo apt-get install postgresql postgis redis mongodb
 
 # Runtimes / Interpreters
-sudo apt-get install node
+sudo apt-get install node python-dev
+
+# NPM
+sudo npm install -g jslint foreman gulp jasmine
