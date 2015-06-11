81892e349cbd08f403f9789caad70ef02d02a639
Conail Stewart <conail@conail.net>
2015-06-11 16:23:16 -0400

    Dev dependencies for new installs.

---
+++ script/global-dev-deps.sh
@@ -0,0 +1,22 @@
+sudo apt-get update
+
+# Common Development Libraries
+sudo apt-get install build-essential tree
+
+# Network
+sudo apt-get install netcat whois
+
+# Source Control
+sudo apt-get install git svn
+
+# Editors
+sudo apt-get install vim emacs
+
+# Third-party Utilities
+sudo apt-get install heroku-toolbelt
+
+# Database
+sudo apt-get install postgresql postgis redis mongodb
+
+# Runtimes / Interpreters
+sudo apt-get install node
