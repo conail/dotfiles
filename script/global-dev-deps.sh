c09ff22cd62489a9f9db3af42b1a920afa99b501
Conail Stewart <conail@conail.net>
2015-06-19 09:51:30 -0400

    Add python dev libs to install script.

--- script/global-dev-deps.sh
+++ script/global-dev-deps.sh
@@ -1,25 +1,25 @@
-sudo apt-get update
+apt-get update
 
 # Common Development Libraries
-sudo apt-get install build-essential tree cmake ctags
+apt-get install build-essential tree cmake ctags libbz2-dev libsqlite3-dev libreadline-dev zlib1g-dev libncurses5-dev libssl-dev libgdbm-dev
 
 # Network
-sudo apt-get install netcat whois wget curl
+apt-get install netcat whois wget curl
 
 # Source Control
-sudo apt-get install git svn
+apt-get install git svn
 
 # Editors
-sudo apt-get install vim emacs23-nox dhex
+apt-get install vim emacs23-nox dhex
 
 # Third-party Utilities
-sudo apt-get install heroku-toolbelt
+apt-get install heroku-toolbelt
 
 # Database
-sudo apt-get install postgresql postgis redis mongodb
+apt-get install postgresql postgis redis mongodb
 
 # Runtimes / Interpreters
-sudo apt-get install node python-dev
+apt-get install node python-dev python-pip
 
 # NPM
-sudo npm install -g jslint foreman gulp jasmine
+npm install -g jslint foreman gulp jasmine
